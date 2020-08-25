05f8630e5aff098843bafe2d8229309742aa680f
ltvnvchil <ltvnvchil@users.noreply.github.com>
2020-08-25 11:56:20 +0700

    Update mainmenu scene
    Add canvas, panel and buttons instead of sprites, add script for switch scenes

---
+++ Hex-game/Assets/Scripts/loadScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadScene(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    // Update is called once per frame
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
